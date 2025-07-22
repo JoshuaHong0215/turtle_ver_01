5357885dc9dfa320b2f9230f2d28f86a1cbac2ab
JoshuaHong0215 <sebin5736@gmail.com>
2025-07-22 11:46:18 +0900

    함수 추가 버전
    custom_forward()라는 def함수를 만들고
    기존의 t.forward(150)을 custom_forward(t, 150)으로 변경하여 자동으로 이동값을 저장하는 기능을 가짐

--- turtle_ver_01_Code_ver_-1.py
+++ turtle_ver_01_Code_ver_-1.py
@@ -35,13 +35,45 @@ t.goto(-350,-300)
 t.setheading(45)
 t.pendown()
 
-# 장애물 회피 기동 
-t.goto(-200,-150)
+# 장애물 회피 기동_ver_01
+# t.forward(100)
+# t.setheading(45)
+# t.forward(500)
+# t.setheading(-45)
+# t.forward(150)
+# t.setheading(45)
+# t.forward(150)
+
+# 장애물 회피 기동_ver_03
+# t.forward(300)
+# t.setheading(135)
+# t.forward(150)
+# t.setheading(45)
+# t.forward(350)
+# t.setheading(-45)
+# t.forward(150)
+# t.setheading(45)
+# t.forward(200)
+
+# 함수 지정
+distance_list = []
+
+def custom_forward(turtle_obj, dist):
+    turtle_obj.forward(dist)
+    distance_list.append(dist)
+# -------------------------------------------
+
+# 장애물 회피 기동_ver_02
+custom_forward(t, 300)
 t.setheading(135)
-t.forward(150)
+custom_forward(t, 150)
 t.setheading(45)
-t.forward(500)
+custom_forward(t, 350)
 t.setheading(-45)
-t.forward(150)
+custom_forward(t, 150)
 t.setheading(45)
-t.forward(150)
+custom_forward(t, 200)
+
+# 이동 거리 출력
+total_distance = sum(distance_list)
+print(f"총 이동 거리: {total_distance} 픽셀")
