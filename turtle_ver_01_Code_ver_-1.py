c5894b3cd31b5546dda7b9e6c8d34e630053fe3c
JoshuaHong0215 <sebin5736@gmail.com>
2025-07-22 10:16:24 +0900

    Detail_add_01
    turtle shape
    turtle line style
    etc

--- turtle_ver_01_Code_ver_-1.py
+++ turtle_ver_01_Code_ver_-1.py
@@ -5,4 +5,19 @@ s = turtle.getscreen()
 
 # 거북이 변수 지정
 t = turtle.Turtle()
+
+# turtle shape style 
 t.shape("turtle")
+t.shapesize(1.5,1.5,1.5)
+
+# turtle line style 
+t.pencolor("purple")
+t.pensize(4)
+
+t.goto(0,0)
+
+# 거북이 이동경로 지정
+t.penup()
+t.goto(-350,-300)
+t.pendown()
+t.goto(350,300)
